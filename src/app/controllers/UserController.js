9355e6058652df52354d16dd41a167d0b0fbe6c2
ebagabee <gabriel.ebagabe2001@gmail.com>
2024-08-06 18:59:59 -0300

    Enviando e-mail sem queue

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,22 @@
+import Mail from "../lib/Mail.js";
+
+export default {
+    async store(req, res) {
+        const { name, email, password } = req.body;
+
+        const user = {
+            name,
+            email,
+            password
+        };
+
+        await Mail.sendMail({
+            from: 'Queue Test <queue@queuetest.com.br>',
+            to: `${name} <${email}>`,
+            subject: 'Cadastro de usuário',
+            html: `Olá. ${name}, bem vindo ao sistema de filas :)`
+        })
+
+        return res.json(user);
+    }
+}
